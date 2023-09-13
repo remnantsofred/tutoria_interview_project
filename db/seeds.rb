9c48d20abe295dc670aecc7635c26a67f82660d3
Safitri (Fifi) Shelton <110148438+fee3fitri@users.noreply.github.com>
2023-09-13 15:29:03 -0700

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Message.create(
+  user: "Bob",
+  body: "Hello! How are you doing today? Always nice to greet someone and start a conversation. I hope this message finds you well and brings a smile to your face."
+)
+
+Message.create(
+  user: "Nelly",
+  body: "Sounds great! When and where would you like to meet? I'm available most weekdays after 5 pm and weekends after 12 pm. Would you prefer to meet for coffee or grab a bite to eat?"
+)
+
+Message.create(
+  user: "Will",
+  body: "Just finished reading an amazing book. Highly recommend it!"
+)
+
+Message.create(
+  user: "Sheila",
+  body: "The project deadline has been extended by one week."
+)
+
+Message.create(
+  user: "Woody",
+  body: "Ahoy!"
+)
